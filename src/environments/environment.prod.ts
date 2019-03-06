6deae7948a9025898e788dc4aa72a7d0695179f2
CorentinDoue <doue.coco@hotmail.fr>
2019-03-06 16:52:48 +0100

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
+  cas_login_url: 'https://cas.emse.fr',
+  api_login_url: 'https://portail-etu.emse.fr/api/login',
+  api_url: 'https://portail-etu.emse.fr/api/api',
+  api_suffix: '/api',
+  img_url: 'https://portail-etu.emse.fr/api/img',
   production: true
 };

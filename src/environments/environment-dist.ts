3f4aa6b8846a3665417999ba45fcda75bac7178e
CorentinDoue <doue.coco@hotmail.fr>
2019-12-28 14:53:10 +0100

    chore: ignore local env

---
+++ src/environments/environment-dist.ts
@@ -0,0 +1,14 @@
+// To copy as environment.ts
+// fill with your local data
+
+export const environment = {
+  cas_login_url: 'https://cas.emse.fr',
+  api_login_url: 'http://local-portail-etu.emse.fr/api/login',
+  api_url: 'http://local-portail-etu.emse.fr/api/api',
+  api_uri: 'api',
+  api_suffix: '/api/api',
+  img_url: 'http://local-portail-etu.emse.fr/api/img',
+  excel_url: 'http://local-portail-etu.emse.fr/api/excel',
+  home: 'http://local-portail-etu.emse.fr/',
+  production: false,
+};

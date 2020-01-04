c6670122445d987b1e55f53025031f0e5b13309b
CorentinDoue <doue.coco@hotmail.fr>
2020-01-04 23:34:18 +0100

    feat: hide events

--- src/app/associations/components/events-list/events-list.component.ts
+++ src/app/associations/components/events-list/events-list.component.ts
@@ -1,18 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Association } from '../../../core/models/association.model';
+import { EventLight } from '../../../core/models/event.model';
 
 @Component({
   selector: 'app-events-list',
   templateUrl: './events-list.component.html',
   styleUrls: ['./events-list.component.scss'],
 })
-export class EventsListComponent implements OnInit {
-  @Input() asso: Association;
+export class EventsListComponent {
+  events: EventLight[] = [];
+  _asso: Association;
+  moreThan10Events: boolean;
+  @Input()
+  set asso(asso: Association) {
+    this._asso = asso;
+    this.moreThan10Events = asso.events.length > 10;
+    this.events = asso.events.slice(Math.max(asso.events.length - 10, 1));
+  }
+  get asso(): Association {
+    return this._asso;
+  }
   @Input() isRightful: boolean;
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
-
-  // eslint-disable-next-line no-empty-function, @typescript-eslint/no-empty-function
-  ngOnInit() {}
+  displayAll(): void {
+    this.events = this.asso.events;
+  }
 }

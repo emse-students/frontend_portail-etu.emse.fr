33bab3272e5da102c354bb6751069685ca838d09
Evolve <Artevolve@yahoo.com>
2020-09-07 14:40:01 +0200

    feat: sponsors assets

--- src/app/core/data/sponsors.data.ts
+++ src/app/core/data/sponsors.data.ts
@@ -3,10 +3,12 @@ import { Sponsor } from '../models/sponsor.model';
 export const SPONSORS_DATA: Sponsor[] = [
   {
     name: 'Bar - BXL',
+    logo: '../../../assets/sponsors/BXL.png',
     incentives: ['-1€ sur les consos (bières et softs hors jus) hors Happy Hour'],
   },
   {
     name: 'Banque - BNP Paribas',
+    logo: '../../../assets/sponsors/BNPParibas.png',
     incentives: [
       '70€ offerts à l’ouverture d’un compte',
       'Carte VISA Gold gratuite',
@@ -15,22 +17,27 @@ export const SPONSORS_DATA: Sponsor[] = [
   },
   {
     name: 'Auto-Ecole - Conduite Passion',
+    logo: '../../../assets/sponsors/ConduitePassion.png',
     incentives: ["Plus de 110e d'économie sur le code, passage du permis gratuit"],
   },
   {
     name: 'Livraison de courses - U Express',
+    logo: '../../../assets/sponsors/UExpress.png',
     incentives: ["Livraison gratuite à la ME avec -5% sur le total à partir de 5€ d'achat"],
   },
   {
     name: 'Cours particuliers - Mymentor',
+    logo: '../../../assets/sponsors/MyMentor.png',
     incentives: ["Possibilité de donner des cours à partir de 20€ de l'heure"],
   },
   {
     name: 'Location de meubles - EcoLoc',
+    logo: '../../../assets/sponsors/EcoLoc.png',
     incentives: ['10% de réduction sur le mobilier avec le code EMSE10'],
   },
   {
     name: 'Location de box - Homebox',
+    logo: '../../../assets/sponsors/HomeBox.png',
     incentives: [
       '20% sur les locations de box et 10% sur le Homeshop (sur présentation de la carte étudiant)',
     ],
